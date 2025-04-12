4a7d5326af040a22eaa3ab50afbe13b800313f16
Ayaan <ayaan.waqas@outlook.com>
2025-04-12 10:39:08 +0100

    got an embed layout working

--- commands/exhang/status.js
+++ commands/exhang/status.js
@@ -1,24 +1,50 @@
 const { SlashCommandBuilder } = require('discord.js');
 
+let embed = {
+  color: 0x3fd9ac,
+  title: 'Executive Hangar Status',
+  fields: [
+    {
+      name: 'Overview',
+      value: ':green_square:  :green_square:  :red_square:  :red_square:  :red_square: - { until open }',
+    },
+    { name: '\u200b', value: '\u200b' },
+    {
+      name: ':green_square: Light 1',
+      value: 'Turned green 25 minutes ago.',
+    },
+    {
+      name: ':green_square: Light 2',
+      value: 'some text here',
+    },
+    {
+      name: ':red_square: Light 3',
+      value: 'Turns green in 23 minutes.',
+    },
+    {
+      name: ':red_square: Light 4',
+      value: 'some text here',
+    },
+    {
+      name: ':red_square: Light 5',
+      value: 'some text here',
+    },
+    { name: '\u200b', value: '\u200b' },
+    {
+      name: 'ExHang { open/closed }',
+      value: '{ opens/closes } in some number of minutes/hours',
+    },
+  ],
+}
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('status')
     .setDescription('Fetches ExHang status'),
   
   async execute(interaction) {
-    await interaction.reply(
-      `
-      this is a test message
-      :green_square: :green_square: :red_square: :red_square: :red_square:
-      
-      Light 1: never
-      Light 2: idk
-      Light 3: maybe if this were actually finished I could tell you
-      Light 4: ...
-      Light 5: ...
-      
-      Next hangar open: ...
-      `.replace(/  +/g, '')
-    );
+    await interaction.reply({
+      embeds: [embed],
+    });
   }
 }
